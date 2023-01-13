c5927ad4e8085ad7a98647735cd490a1be1a5572
Kauan <kauan.s.leite@hotmail.com>
2023-01-13 18:11:54 -0300

    3° Requisito

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -34,6 +34,8 @@ describe('Car', function () {
     const result = await service.create(carInput);
 
     expect(result).to.be.deep.equal(carOutput);
+
+    sinon.restore();
   });
 
   it('List All Cars in DB', async function () {
@@ -54,5 +56,46 @@ describe('Car', function () {
     const result = await service.getAll();
 
     expect(result).to.be.deep.equal([carOutput]);
+
+    sinon.restore();
+  });
+
+  it('List Cars in DB by ID with SUCCESS', async function () {
+    const carOutput: Car = new Car({
+      id: '63c1c10301249251caccc688',
+      model: 'Corsa',
+      year: 2001,
+      color: 'gray',
+      status: true,
+      buyValue: 10000,
+      doorsQty: 2,
+      seatsQty: 5,
+    });
+
+    sinon.stub(Model, 'findById').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.getByID('63c1c10301249251caccc688');
+
+    expect(result).to.be.deep.equal(carOutput);
+
+    sinon.restore();
+  });
+
+  it('List Cars in DB by ID with FAILED', async function () {
+    sinon.stub(Model, 'findById').resolves();
+
+    const service = new CarService();
+    const resultA = await service.getByID('63c1c10301249251caccc688');
+
+    expect(resultA).to.be.deep.equal('NOT_FOUND');
+
+    sinon.restore();
+
+    sinon.stub(Model, 'findById').resolves(null);
+    const resultB = await service.getByID('63c1c10301249251caccc688');
+    expect(resultB).to.be.deep.equal('NOT_FOUND');
+
+    sinon.restore();
   });
 });
